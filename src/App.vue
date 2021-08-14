1a1832d67f99c6d20db1bc877510e224becb08e2
abhishekyocket <abhishek.janjalkar@yocket.in>
2021-08-15 02:09:38 +0530

    Conditional array iterating

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <div>
-    <h2 v-for="(name, index) in names" :key="name">{{index}} {{name}}</h2>
-    <h2 v-for="name in fullNames"  :key="name.first">{{name.first}} {{name.last}}</h2>
-    <div v-for="actor in actors"  :key="actor.names">
-      <h2>{{actor.name}}</h2>
-      <h3 v-for="movie in actor.movies" :key="movie">{{movie}}</h3>
-    </div>
-
-    <h2 v-for="(value, key, index) in myInfo" :key="value">{{index}} {{key}} {{value}}</h2>
-
-    <template v-for="name in names" :key="name">
-      <h2>{{name}}</h2>
-      <hr/>
-    </template>
-
-  </div>
-
+<div>
+  <template v-for="name in names" >
+    <h2 v-if="name==='Diana'" :key="name">{{name}}: </h2>
+    
+  </template><br>
+  <button @click="shuffle">Shuffle!</button>
+</div>
 </template>
 
 <script>
@@ -24,23 +14,7 @@ export default {
   name: 'App',
   data() {
     return {
-      names: ['Bruce', 'Clark', 'Diana'],
-      fullNames: [
-        { first: 'Bruce', last: 'Wayne' },
-        { first: 'Clark', last: 'Kent' },
-        { first: 'Princess', last: 'Diana' }
-      ],
-
-      actors: [
-        {name: 'Christian Bale', movies: ['Batman', 'The prestige']},
-        {name: 'Di Caprio', movies: ['Titanic', 'Inception']},
-      ],
-
-      myInfo: {
-        name: 'Abhishek',
-        channel: 'Weardo',
-        course: 'Google'
-      }
+      names: ['Bruce', 'Clark', 'Diana', 'Barry'],
     };
   },
 };
